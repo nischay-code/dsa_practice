39a9cc5245a8cda9c66786ed54a142c69c9dd6bb
nischay-code <nischay.patel.code@gmail.com>
2022-07-08 10:59:19 +0530

    for each loop

--- array_practice.cpp
+++ array_practice.cpp
@@ -11,8 +11,15 @@ int main()
     cout<<a[1]<<endl;
     cout<<a[2]<<endl;*/
 
-    for (int i = 0; i < 9; i++)
+    /*for (int i = 0; i < 9; i++)
     {
         printf("%d\n", a[i]);
+    }*/
+
+    // for each loop
+
+    for (int x : a)
+    {
+        printf("%d\n", x);
     }
 }
