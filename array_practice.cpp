e15321b4a8a56a42dcf3f26bb1f48f2ed7676797
nischay-code <nischay.patel.code@gmail.com>
2022-07-08 10:49:41 +0530

    practicing

---
+++ array_practice.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a[5];
+    a[0]=5;
+    a[1]=4;
+    a[2]=1;
+
+    cout<<sizeof(a)<<endl;  
+    cout<<a[1]<<endl;  
+    cout<<a[2]<<endl;  
+    
+}
