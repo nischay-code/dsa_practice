5433fb5c8ac3469588b79229963604ba5efe9a1e
nischay-code <nischay.patel.code@gmail.com>
2022-07-08 10:55:34 +0530

    using for loop

--- array_practice.cpp
+++ array_practice.cpp
@@ -1,13 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-    int a[5];
-    a[0]=5;
-    a[1]=4;
-    a[2]=1;
+int main()
+{
+    int a[9];
+    a[0] = 5;
+    a[1] = 4;
+    a[2] = 1;
 
-    cout<<sizeof(a)<<endl;  
-    cout<<a[1]<<endl;  
-    cout<<a[2]<<endl;  
-    
+    /*cout<<sizeof(a)<<endl;
+    cout<<a[1]<<endl;
+    cout<<a[2]<<endl;*/
+
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d\n", a[i]);
+    }
 }
